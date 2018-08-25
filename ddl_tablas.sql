445422c85edf8e3be2bd2d072670791be6262d3a
gonzalo <gon2794@gmail.com>
2018-08-25 12:17:55 -0500

    version final

--- ddl_tablas.sql
+++ ddl_tablas.sql
@@ -5,4 +5,8 @@ objetivo: creacion de objetos
 
 fecha: 25/08
 
----prueba
+---pruebas
+----- correccion de cancelacion en produccion
+create table t_clientes(
+	nombre varchar2(30),
+	fecha date)table_space;
